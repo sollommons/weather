de4caf469d54c2573c7626440e47f14a65f2c35f
Churina Margaery <elshanskaya.r@inbox.ru>
2025-03-05 11:31:29 +0300

    app split

---
+++ frontend/src/components/app/app.tsx
@@ -0,0 +1,20 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { Fragment } from 'react/jsx-runtime';
+
+
+
+function App(): JSX.Element {
+  // const authorizationStatus = AuthorizationStatus.Unknown;
+
+  // if (authorizationStatus === AuthorizationStatus.Unknown || isOffersDataLoading) {
+  //   return (
+  //     <LoadingScreen />
+  //   );
+  // }
+
+  return (
+    <Fragment />
+  );
+}
+
+export default App;
