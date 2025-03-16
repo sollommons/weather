700cb54110e8999adc13a18de6a8f2fc1346e380
sol <sasha.kudryavtsev.20@gmail.com>
2025-03-16 13:18:30 +0300

    Create test repo

---
+++ ui_test/src/test/java/com/example/BaseTest.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static com.codeborne.selenide.Browsers.CHROME;
+import static com.codeborne.selenide.WebDriverRunner.clearBrowserCache;
+
+public class BaseTest {
+    @BeforeEach
+    public void setupConf() {
+        Configuration.browser = CHROME;
+        Configuration.baseUrl = "http://localhost:5173/";
+        Selenide.open("/");
+    }
+
+    @AfterEach
+    public void endJob() {
+        clearBrowserCache();
+        Selenide.closeWebDriver();
+    }
+}
