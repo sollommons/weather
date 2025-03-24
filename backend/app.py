4e72c738e6d906f011962b41ba3f7600ae7d6f5f
kristina <kristina.bykova.03@list.ru>
2025-03-24 15:23:34 +0300

    add code 200 or 404

--- backend/app.py
+++ backend/app.py
@@ -6,14 +6,19 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     data = None
+    error = None
     if request.method == 'POST':
         city = request.form['cityName']
         state = request.form['stateName']
         country = request.form['countryName']
-        data = get_weather(city, state, country)
-        #print(get_weather(city, state, country))
+        result, status = get_weather(city, state, country)
+        if status == 200:
+            data = result
+        else:
+	    error = result['error']
+	#print(get_weather(city, state, country))
 
-    return render_template('index.html', data=data)
+    return render_template('index.html', data=data, error=error)
 
 if __name__ == '__main__':
     app.run(debug=True)
