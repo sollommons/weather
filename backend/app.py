6a851cc9a2bb19abd31e58b2af2e76c2b31bf74b
kristina <kristina.bykova.03@list.ru>
2025-04-03 16:47:32 +0300

    format

--- backend/app.py
+++ backend/app.py
@@ -8,21 +8,17 @@ def index():
     data = None
     error = None
     if request.method == 'POST':
-        location = request.form['cityName']
-        parts = location.split(',')
-        if len(parts) == 1:
-            city = parts[0].strip()
-            region = None
-        else:
-            city = parts[0].strip()
-            region = parts[1].strip()
-        result, status_code = get_weather(city, region)
-        if status == 200:
-            data = result
-        else:
-            error = result['error']
-
-    return render_template('index.html', data=data, error=error)
+        city, _, region = request.form['cityName'].strip().partition(',')
+        city = city.strip()
+        region = region.strip() if region else None
+        try:
+            current_weather_json, forecast_json, status_code = get_weather(city, region)
+        except (ValueError, TypeError):
+            status_code = get_weather(city, region)
+            current_weather_json = forecast_json = None
+        
+        #print(current_weather_json, forecast_json, status_code)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- backend/weather.py
+++ backend/weather.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 import os
 from dataclasses import dataclass
 from datetime import datetime, timezone
-from timezonefinder import TimezoneFinder
-import pytz
+import json
+from requests.exceptions import Timeout
 
 load_dotenv()
 api_key = os.getenv('keyWeather')
@@ -16,8 +16,8 @@ class WeatherData:
     icon: str
     wind_speed: float
     visibility: float
-    sunrise: int
-    sunset: int
+    sunrise: str
+    sunset: str
     humidity: int
     pressure: int
 
@@ -27,38 +27,32 @@ def get_lan_lon(API_key, city_name, region):
     else:
         location = city_name
     country_name = "RU"
-    resp = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={location},{country_name}&appid={API_key}')
-    print(resp.status_code)
-    if resp.status_code == 200:
-        data = resp.json()
-        print(data)
-        return (data[0].get('lat'), data[0].get('lon'), 200) if data else (None, None, 201) 
-    else:
-        return None, None, resp.status_code  
+     try:
+        resp = requests.get(
+            f'http://api.openweathermap.org/geo/1.0/direct?q={location},{country_name}&appid={API_key}',
+            timeout=(10, 10)
+        )
+        if resp.status_code == 200:
+            data = resp.json()
+            return (data[0].get('lat'), data[0].get('lon'), 200) if data else (None, None, 400)
+        else:
+            return None, None, resp.status_code
+    except Timeout:
+        return None, None, 408
+    except requests.exceptions.RequestException as e:
+        return None, None, 500
 
 def get_current_weather(lat, lon, API_key):
     resp = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}&units=metric').json()
     
-    visibility_km = resp.get('visibility') / 1000 
- 
-    tf = TimezoneFinder()
-    timezone_str = tf.timezone_at(lat=lat, lng=lon)  # Определяем часовой пояс по координатам
-    local_tz = pytz.timezone(timezone_str)
-
-    sunrise_timestamp = resp.get('sys').get('sunrise')
-    sunset_timestamp = resp.get('sys').get('sunset')
-    
-    sunrise_hour = datetime.fromtimestamp(sunrise_timestamp, tz=pytz.utc).astimezone(local_tz).strftime('%H:%M')
-    sunset_hour = datetime.fromtimestamp(sunset_timestamp, tz=pytz.utc).astimezone(local_tz).strftime('%H:%M')
-
     data = WeatherData(
         description=resp.get('weather')[0].get('description'),
         icon=resp.get('weather')[0].get('icon'),
         temperature=int(resp.get('main').get('temp')),
         wind_speed=resp.get('wind').get('speed'),
-        visibility=visibility_km,
-        sunrise=sunrise_hour,
-        sunset=sunset_hour,
+        visibility=resp.get('visibility') / 1000,
+        sunrise=datetime.fromtimestamp(resp['sys']['sunrise'], tz=timezone.utc).isoformat(timespec='seconds').replace('+00:00', 'Z'),
+        sunset=datetime.fromtimestamp(resp['sys']['sunset'], tz=timezone.utc).isoformat(timespec='seconds').replace('+00:00', 'Z'),
         humidity=resp.get('main').get('humidity'),
         pressure=resp.get('main').get('pressure')
     )
@@ -67,6 +61,7 @@ def get_current_weather(lat, lon, API_key):
 
 @dataclass
 class WeatherForecastData:
+    time: str
     temperature: float
 
 def get_weather_forecast(lat, lon, API_key):
@@ -75,7 +70,8 @@ def get_weather_forecast(lat, lon, API_key):
     
     for entry in resp.get('list', [])[:5]:
         forecast = WeatherForecastData(
-            temperature=int(entry.get('main').get('temp'))
+            temperature=int(entry.get('main').get('temp')),
+            time=datetime.fromtimestamp(entry['dt'], tz=timezone.utc).isoformat(timespec='seconds').replace('+00:00', 'Z')
         )
         forecasts.append(forecast)
     
@@ -83,13 +79,30 @@ def get_weather_forecast(lat, lon, API_key):
 
 def main(city_name, region):
     lat, lon, status_code = get_lan_lon(api_key, city_name, region)
-    print(status_code)
     if status_code == 200:
         weather_data = get_current_weather(lat, lon, api_key)
         forecast_data = get_weather_forecast(lat, lon, api_key)
-        return {"weather": weather_data, "forecast": forecast_data}, status_code
-    elif status_code == 201:
-         return {"error": f"Error location "}, status_code
+        current_weather = {
+            "temperature": weather_data.temperature,
+            "wind_speed": weather_data.wind_speed,
+            "visibility": weather_data.visibility,
+            "pressure": weather_data.pressure,
+            "humidity": weather_data.humidity,
+            "sunrise": weather_data.sunrise,
+            "sunset": weather_data.sunset,
+            "description": weather_data.description,
+            "icon": f"https://openweathermap.org/img/wn/{weather_data.icon}@2x.png"
+        }
+        current_weather = json.dumps(current_weather, ensure_ascii=False)
+        forecast_list = []
+        for forecast in forecast_data:
+
+            forecast_list.append({
+                "date": forecast.time,
+                "temperature": forecast.temperature
+            })
+        forecasts = json.dumps(forecast_list, ensure_ascii=False)
+        return current_weather, forecasts, status_code
     else:
-        return {"error": f"Error response "}, status_code
+        return status_code
 
